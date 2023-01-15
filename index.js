e47dafd96f5f15b3059c470ed1d3c1c8646e3dd2
RafatAiub <tanvirmahtab1996@gmail.com>
2023-01-15 20:54:54 +0600

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-serve");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middle_wares = jsonServer.default();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3200;
 
 server.use(middle_wares);
 server.use(router);
